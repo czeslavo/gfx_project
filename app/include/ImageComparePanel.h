1b800e8b036c2b549be49777c076753508e5c2f5
Grzegorz Burzyński <czeslavo@gmail.com>
2017-01-23 01:02:53 +0100

    [App] Now images are draggable

--- app/include/ImageComparePanel.h
+++ app/include/ImageComparePanel.h
@@ -16,10 +16,25 @@ public:
     void paintNow();
     void draw(wxDC& dc);
 
+    void onStartDragging(wxMouseEvent& event);
+    void onDrag(wxMouseEvent& event);
+    void onEndDragging(wxMouseEvent& event);
+
     void registerImageService(std::shared_ptr<core::ImageService> service);
 
 private:
     void registerEventHandlers();
+
+    struct {
+        bool dragging{false};
+        int x0;
+        int y0;
+    } dragInfo;
+
+    struct {
+        int x;
+        int y;
+    } imagePosition;
 
     std::shared_ptr<core::ImageService> imageService;
 };
--- app/source/ImageComparePanel.cpp
+++ app/source/ImageComparePanel.cpp
@@ -14,6 +14,10 @@ ImageComparePanel::ImageComparePanel(wxWindow* parent)
 void ImageComparePanel::registerEventHandlers()
 {
     Bind(wxEVT_PAINT, &ImageComparePanel::onPaint, this);
+
+    Bind(wxEVT_LEFT_DOWN, &ImageComparePanel::onStartDragging, this);
+    Bind(wxEVT_MOTION, &ImageComparePanel::onDrag, this);
+    Bind(wxEVT_LEFT_UP, &ImageComparePanel::onEndDragging, this);
 }
 
 void ImageComparePanel::onPaint(wxPaintEvent& event)
@@ -37,7 +41,34 @@ void ImageComparePanel::draw(wxDC& dc)
     if (not bitmap.IsOk())
         return;
 
-    dc.DrawBitmap(bitmap, 0, 0);
+    dc.DrawBitmap(bitmap, imagePosition.x, imagePosition.y);
+}
+
+void ImageComparePanel::onStartDragging(wxMouseEvent& event)
+{
+    if (not dragInfo.dragging)
+    {
+        dragInfo.dragging = true;
+        dragInfo.x0 = imagePosition.x + event.GetX();
+        dragInfo.y0 = imagePosition.y + event.GetY();
+    }
+}
+
+void ImageComparePanel::onDrag(wxMouseEvent& event)
+{
+    if (not (event.LeftIsDown() and dragInfo.dragging))
+        return;
+
+    imagePosition.x = dragInfo.x0 - event.GetX();
+    imagePosition.y = dragInfo.y0 - event.GetY();
+
+    paintNow();
+}
+
+void ImageComparePanel::onEndDragging(wxMouseEvent& event)
+{
+    dragInfo.dragging = false;
+
 }
 
 void ImageComparePanel::registerImageService(std::shared_ptr<core::ImageService> service)
